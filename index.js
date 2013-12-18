e7df650322f3160b6b061e3fdc8c15b3295335a9
tmkctm <tmkctm@gmail.com>
2013-12-18 13:44:45 +0200

    keys tracker in console

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-/**
- * Created with JetBrains WebStorm.
- * User: admin
- * Date: 11/25/13
- * Time: 1:03 PM
- * To change this template use File | Settings | File Templates.
- */
-
 require('./categoriesMap.js');
 
 var fs = require('fs');
 global.keys = (global.keys) ? global.keys : {};
 
 var data = JSON.parse(fs.readFileSync(__dirname+"/keys.loc"));
+console.log('read keys from file '+__dirname+"/keys.loc"+': '+JSON.stringify(data));
 global.keys['apiKey'] = data['shopzillaApiKey'];
 global.keys['publisherId'] = data['shopzillaPublisherID'];
 global.keys['shoppingApiKey'] = data['shoppingApiKey'];
